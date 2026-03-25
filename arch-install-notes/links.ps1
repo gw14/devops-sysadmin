8a2fcc7acdf857df23b337a73f5e2f0001a6fdb6
$$hiMMi <waldmanguy@gmail.com>
2026-03-25 10:42:56 +0200

    Add PowerShell script to open various links

---
+++ arch-install-notes/links.ps1
@@ -0,0 +1,8 @@
+#open roboform:
+start-process "https://online.roboform.com"
+
+#open mail:
+start-process "https://mail.google.com/mail/u/0/#inbox"
+
+#open the installation guide:
+start-process "https://wiki.archlinux.org/title/Installation_guide"
